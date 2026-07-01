2b864589fb532525132ddb6a7a206dbc0fba1453
vivaan91c <vivaan91c@gmail.com>
2026-07-01 13:08:17 +0530

    add string methods practice

---
+++ string_methods.py
@@ -0,0 +1,21 @@
+# String Methods Practice
+# Topics: upper, lower, capitalize, len, replace
+# Author: vivaan chauhan
+
+name = "VIvAAn chAUHAN"
+preference = "I like dogs, dogs are very cute"
+
+new_name = name.upper()
+print(new_name)
+
+new_name = name.lower()
+print(new_name)
+
+new_name = name.capitalize()
+print(new_name)
+
+new_prefernce = preference.replace("dogs", "cats")
+print(new_prefernce)
+
+length = len(new_prefernce)
+print(length)
