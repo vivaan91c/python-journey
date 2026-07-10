567f8a3906fdf42629db3614aeddc3f11c447fcd
Vivaan Chauhan <vivaan91c@gmail.com>
2026-07-10 22:17:43 +0530

    add functions practice

---
+++ functions.py
@@ -0,0 +1,26 @@
+# functions using if/elif
+
+def checkDriverAge():
+    age = int(input("What is your age?: "))
+
+    if int(age) < 18:
+        print("Sorry, you are too young to drive this car. Powering off")
+    elif int(age) > 18:
+        print("Powering On. Enjoy the ride!");
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride!")
+
+checkDriverAge()
+
+
+# OR
+
+
+def checkDriverAge(age=0):
+    if int(age) < 18:
+        print("Sorry, you are too yound to drive this car. Powering off")
+    elif int(age) > 18:
+        print("Powering On. Enjoy the ride!");
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride!")
+checkDriverAge()
