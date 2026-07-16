69293bb0571413dcb8320971931c6bb6142c22ce
Vivaan Chauhan <vivaan91c@gmail.com>
2026-07-16 17:28:09 +0530

    add class method - Inheritance

---
+++ Inheritance.py
@@ -0,0 +1,24 @@
+class User():
+    def sign_in(self):
+        print("logged in")
+
+class Wizard(User):
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        print(f"attacking with power of {self.power}")
+
+class Archer(User):
+    def __init__(self, name, num_arrows):
+        self.name = name
+        self.num_arrows = num_arrows
+
+    def attack(self):
+        print(f"attacking with arrows: arrows left - {self.num_arrows} ")
+        
+Wizard1 = Wizard("vivaan", 50)
+Archer1 = Archer("Robin", 70)
+Wizard1.attack()
+Archer1.attack()
